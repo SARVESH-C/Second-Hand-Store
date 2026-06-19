a1eb56b92b6559f5e2a0ec015ab06c7e606e11dc
Sarvesh <sarveshch2004@gmail.com>
2026-06-19 23:01:09 +0530

    Security & Repositories

---
+++ src/main/java/com/secondhand/security/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package com.secondhand.security;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.ecpiration")
+    private long jwtExpirationMs;
+
+    public String generateToken(Long userId) {
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+    public Long getUserIdFromToken(String token){
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        String userId = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+            return Long.parseLong(userId);
+                
+    }
+    public boolean validateToken(String token){
+        try{
+            SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+            Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    
+}
