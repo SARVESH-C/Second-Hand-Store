37ed96dccdf72e7ac6dd2657486729e7cb78edec
Sarvesh <sarveshch2004@gmail.com>
2026-06-20 23:35:16 +0530

    Security & configuration

---
+++ src/main/java/com/secondhand/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.secondhand.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException{
+        try{
+            String authHeader = request.getHeader("Authorization");
+            if(authHeader != null && authHeader.startsWith("Bearer")){
+                String token = authHeader.substring(7);
+                if(jwtTokenProvider.validateToken(token)){
+                    Long userId = jwtTokenProvider.getUserIdFromToken(token);
+                    UsernamePasswordAuthenticationToken auth = 
+                    new UsernamePasswordAuthenticationToken(userId,null,null);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch (Exception e){
+            logger.error("Cannot set user authentication:{}",e);
+        }
+        filterChain.doFilter(request,response);
+                                    
+    }
+    
+    
+}
